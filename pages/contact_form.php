c35eb011326dbed2f2f24a7b4588dd8701f3acf9
emme-code <fenettles@gmail.com>
2022-02-13 18:18:57 -0700

    Updated contact form

--- pages/contact_form.php
+++ pages/contact_form.php
@@ -1,13 +1,11 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
-$email_from = 'fenettles@gmail.com';
-$email_subject = "New Contact";
-$email_body = "You have received a new message from the user $name.\n".
-              "Here is the message:\n $message".
-$to = "fenettles@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $email_from = "fenettles@gmail.com";
+    $email_subject = "New Contact";
+    $email_body = "Message from $name. \n email: $email \n Here is the message: \n $message";
+    $to = "fenettles@gmail.com";
+    $headers = "From: $email_from \r\n";
+    mail($to, $email_subject, $email_body, $headers);
 ?>
